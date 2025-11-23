fa8130da65330d2296d3c58f44773cc3cda566c5
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-11-22 19:38:06 -0500

    feat: Added document downloading to the download button

--- js/index.js
+++ js/index.js
@@ -10,3 +10,24 @@ if (viewTheme) {
 } else {
   console.error('Theme toggle NOT found!');
 }
+
+const downloadCheckbox = document.querySelector('#download-checkbox');
+const fileName = 'Resume.pdf';
+const filePath = 'Resume.pdf';
+
+if (downloadCheckbox) {
+  console.log('Download checkbox found!');
+
+  downloadCheckbox.addEventListener('change', function () {
+    if (this.checked) {
+      console.log('Starting download...');
+
+      const anchor = document.createElement('a');
+      anchor.href = filePath;
+      anchor.download = fileName;
+      document.body.appendChild(anchor);
+      anchor.click();
+      document.body.removeChild(anchor);
+    }
+  });
+}
